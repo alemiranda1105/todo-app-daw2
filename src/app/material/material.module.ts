9c9965cb53003062e55eaf9caac1fd30f5b174af
Alejandro Miranda <amiranda110500@gmail.com>
2022-04-05 15:41:26 +0100

    Task details

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
@@ -20,6 +21,7 @@ import {MatIconModule} from '@angular/material/icon';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule { }
